3a141e2dbbcbe8e830425992e9615c767c5c8920
fomgleb <fomglib@gmail.com>
2024-09-11 20:51:09 +0300

    Rename namespace alias: console_input -> ci

--- server/src/main.cc
+++ server/src/main.cc
@@ -1,14 +1,14 @@
 #include "server/inc/global_room.h"
 #include "utils/inc/console_input.h"
 
-namespace console_input = pon_chat::utils::console_input;
+namespace ci = pon_chat::utils::console_input;
 namespace msock = MinimalSocket;
 
 int main() {
   pon_chat::server::GlobalRoom global_room;
 
-  auto listening_port = console_input::ReadNumber<msock::Port>(
-      "Enter a port to listen: ", "Invalid input");
+  auto listening_port =
+      ci::ReadNumber<msock::Port>("Enter a port to listen: ", "Invalid input");
 
   global_room.Start(listening_port);
 

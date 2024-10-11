c838517afec7f83208c2b1229efdd5ccf2a260b2
fomgleb <fomglib@gmail.com>
2024-10-11 22:23:53 +0300

    Fix argument name in GetAndSetSighandler

--- src/signals/src/signals.cc
+++ src/signals/src/signals.cc
@@ -20,7 +20,7 @@ sighandler_t GetAndSetSighandler(const int signal_number,
     struct sigaction new_action;
     std::memset(&new_action, 0, sizeof(new_action));
     new_action.sa_handler = new_sighandler;
-    if (sigaction(signal, &new_action, &old_action) == -1) {
+    if (sigaction(signal_number, &new_action, &old_action) == -1) {
         std::cerr << "Failed to set the signal handler "
                   << reinterpret_cast<void*>(new_sighandler) << " for signal "
                   << signal << ". Error: " << strerror(errno) << std::endl;
